6692fffe5b06ae1fd8776eb036e5ca79f4993302
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-03-21 09:10:30 +0000

    added a function to update the stock worksheet

--- run.py
+++ run.py
@@ -96,6 +96,22 @@ def last_5_entries_sales():
 last_5_entries_sales()
 
 
+def calculate_stock_data(data):
+    """
+        Calculates the stock data
+    """
+    print('Calculating stock data.... \n')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
         Run all program functions
@@ -105,9 +121,10 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(sales_data, 'surplus')
+    sales_columns = last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 
 print('Welcome to Love Sandwiches data automation')
 main()
-
-sales_columns = last_5_entries_sales()
